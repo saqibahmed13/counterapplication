702e453572f5bd1e12a1472b0ba7f897911b1731
Mohammed Saqib Ahmed <MohammedSaqib.Ahmed@viatris.com>
2023-10-10 15:28:35 +0530

    adding react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const[count, setCount] = useState(0);
+
+  function decreaseCount(){
+    setCount(count-1);
+  }
+
+  function increaseCount(){
+    setCount(count+1);
+  }
+
+  function resetCount(){
+    setCount(0);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
+      <div className='text-[#0398d4] font-medium'>Increment & Decrement</div>
+      <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344145]' >
+        <button onClick={decreaseCount} className='border-r-2 text-center w-20 border-[bfbfbf] text-5xl'>
+          -
+        </button>
+        <div className='font-bold gap-12 text-5xl'>
+          {count}
+        </div>
+
+        
+        <button onClick={increaseCount} className='border-l-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+          +
+        </button>
+      </div>
+      <button onClick={resetCount} className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg'>Reset</button>
     </div>
   );
 }
